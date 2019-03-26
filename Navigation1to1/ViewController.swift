051547f7f7fa4c87870b362ce5bc29af5486a67a
lee-yujinn <leeyj7988@gmail.com>
2019-03-26 17:38:59 +0900

    firstcommit
    1

---
+++ Navigation1to1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Navigation1to1
+//
+//  Created by SWUCOMPUTER on 26/03/2019.
+//  Copyright © 2019 SWUCOMPUTER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    
+    }
+
+
+}
+
